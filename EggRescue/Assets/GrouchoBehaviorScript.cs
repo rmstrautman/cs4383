ad18b4c5e3811bb8d6b7373eab0aed2b840f4f0e
U-computer\billy <billy@computer.(none)>
2014-03-22 17:31:34 -0500

    Added models

---
+++ EggRescue/Assets/GrouchoBehaviorScript.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GrouchoBehaviorScript : MonoBehaviour {
+
+	public Animation walk;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKey("w")) {
+			walk.Play();
+		}
+	}
+}
