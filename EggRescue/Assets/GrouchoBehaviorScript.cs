8b9d0f8ff1344258b6345298a428041e508c5d65
U-computer\billy <billy@computer.(none)>
2014-03-25 17:58:38 -0500

    Added terrain and lots of mushrooms

--- EggRescue/Assets/GrouchoBehaviorScript.cs
+++ EggRescue/Assets/GrouchoBehaviorScript.cs
@@ -7,7 +7,10 @@ public class GrouchoBehaviorScript : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
-	
+		Vector2 SS = renderer.material.mainTextureScale;
+		SS.x = transform.localScale.x;
+		SS.y = transform.localScale.y;
+		renderer.material.mainTextureScale = SS;
 	}
 	
 	// Update is called once per frame
---
+++ EggRescue/Assets/Step1Script.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Step1Script : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		Vector2 SS = renderer.material.mainTextureScale;
+		SS.x = transform.localScale.x / 8;
+		SS.y = transform.localScale.y / 8;
+		renderer.material.mainTextureScale = SS;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
