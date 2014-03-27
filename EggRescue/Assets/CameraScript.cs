c3d356242d607d5b09eaa69cc3201d6c5db03a34
U-computer\billy <billy@computer.(none)>
2014-03-26 23:12:07 -0500

    Raisin man in mushroom land?

---
+++ EggRescue/Assets/CameraScript.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraScript : MonoBehaviour {
+
+	public Transform target;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void LateUpdate () {
+
+		//transform.LookAt (target);
+
+	}
+
+	void Update() {
+
+		transform.position = new Vector3 (target.position.x - 10.0f, target.position.y + 10.0f, target.position.z - 10.0f);
+
+		}
+}
--- EggRescue/Assets/Step1Script.cs
+++ EggRescue/Assets/Step1Script.cs
@@ -13,6 +13,6 @@ public class Step1Script : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-	
+		
 	}
 }
